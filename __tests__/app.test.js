02a52b7a565dd7d65ee1c9d00f596fa46d63f4fd
EmilyDSarani <emily.sarani@gmail.com>
2022-02-07 19:40:49 -0600

    have a generalized plan for tomorrow

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,11 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+//need to import UserService
+//need to create dummy user
+//need to look at notes as to how to do the user and agent stuff
+
+
 describe('backend routes', () => {
   beforeEach(() => {
     return setup(pool);
@@ -11,4 +16,8 @@ describe('backend routes', () => {
   afterAll(() => {
     pool.end();
   });
+  //creates a new user test goes here
+
+
+
 });
